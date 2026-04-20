dcc8529ada73f0d472ded0ef84e29ee0a1a996f7
vinod kumar javaji <javaji.kumar@hcltech.com>
2026-04-20 18:11:00 +0530

    Updated render port

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,9 @@ const axios = require("axios");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+app.get("/", (req, res) => {
+  res.send("Backend Running Successfully");
+});
 
 app.get("/data", async (req, res) => {
   try {
@@ -33,6 +35,8 @@ app.get("/data", async (req, res) => {
   }
 });
 
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
