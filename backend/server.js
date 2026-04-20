277dc51eedbaaf8d62f65199cf61ef67912780d3
vinod kumar javaji <javaji.kumar@hcltech.com>
2026-04-20 18:59:43 +0530

    Enabled CORS

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ const axios = require("axios");
 const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.get("/", (req, res) => {
   res.send("Backend Running Successfully");
 });
