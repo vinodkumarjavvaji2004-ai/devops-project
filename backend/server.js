c395612aec2cf77701544d17535a5ae34c607d82
vinod kumar javaji <javaji.kumar@hcltech.com>
2026-04-20 19:05:26 +0530

    Final CORS fix

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const axios = require("axios");
 const cors = require("cors");
 
 const app = express();
+
 app.use(cors());
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Backend Running Successfully");
 });
